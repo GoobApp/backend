25466929826b66008d18ebb61693c916f6eb242d
Precontation <precontation@proton.me>
2026-04-03 19:14:26 -0700

    switch ai provider to OpenRouter because Groq's a dying company unfortunately

--- server.ts
+++ server.ts
@@ -488,7 +488,7 @@ io.on("connection", (socket: Socket) => {
         if (responseError) {
           console.error(
             "Could not update message (just couldn't idk): " +
-              responseError.message,
+            responseError.message,
           );
         } else {
           io.to(groupId).emit("dm message edited", newId, newContent);
@@ -637,7 +637,7 @@ io.on("connection", (socket: Socket) => {
           .select("message_id");
 
         if (!data) {
-          console.error("Goofy goober Supabase insert error");
+          console.error("Goofy goober Supabase insert error: ", error);
           return;
         }
 
